a53bf1af2e575d271027fa0fa8fd594897b2c9f3
imsparvathaneni-ww <sparvathaneni@innominds.com>
2020-08-20 20:23:28 +0530

    Update config.js

--- public/config.js
+++ public/config.js
@@ -7,7 +7,7 @@ var app = require('express');
 var db_config = {
    user: 'root',
    password: 'root',
-   server: 'localhost', // You can use 'localhost\\instance' to connect to named instance
+   host: 'localhost', // You can use 'localhost\\instance' to connect to named instance
    database: 'myfirstdatabase',
    port: 3306
 }
